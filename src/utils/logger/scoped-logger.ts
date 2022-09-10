6772a94e54dc2626ac11853fc5ffad06d5ec832a
nelson <kamganelson@gmail.com>
2022-09-10 14:09:12 +0000

    feat: add scoped logger

---
+++ src/utils/logger/scoped-logger.ts
@@ -0,0 +1,33 @@
+import { logger } from '@utils/logger/logger';
+
+export class ScopedLogger {
+  private scope: string;
+
+  constructor(scope: string) {
+    this.scope = scope;
+  }
+
+  public debug(message: string): void {
+    this.log('debug', message);
+  }
+
+  public info(message: string): void {
+    this.log('info', message);
+  }
+
+  public warn(message: string): void {
+    this.log('warn', message);
+  }
+
+  public error(message: string): void {
+    this.log('error', message);
+  }
+
+  private log(level: string, message: string): void {
+    logger.log(level, `${this.formatScope()} ${message}`);
+  }
+
+  private formatScope(): string {
+    return `[${this.scope}]`;
+  }
+}
